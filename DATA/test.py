4d9f5e4c5c5f0a0bde90d2c03cd358d21b3cf47f
Maya Saietz <mayasaietz@gmail.com>
2013-12-20 12:33:45 +0100

    Added a bunch of tests.

--- DATA/test.py
+++ DATA/test.py
@@ -12,16 +12,43 @@ def test(command,outp):
 
 def runTests(tests,rootdir):
     for (t,(inp,outp)) in tests.items():
-        runStr = "echo \"" + inp + "\" | ./Paladim -ti " + rootdir + t + ".pal"
-        print "Interpreting " + t + " : " + str(test(runStr, outp))
+        t_ = t.split("_")[0]
+        runStr = "echo \"" + inp + "\" | ./Paladim -ti " + rootdir + t_ + ".pal"
+        print "Interpreting " + rootdir + t + " : " + str(test(runStr, outp))
 
-        compStr = "echo \"" + inp + "\" | ./marsrun " + rootdir + t
-        print "Compiling    " + t + " : " + str(test(runStr, outp))
+        compStr = "echo \"" + inp + "\" | ./marsrun " + rootdir + t_
+        print "Compiling    " + rootdir + t + " : " + str(test(runStr, outp))
 
+def readTest():
+    readtestRes = """Welcome to the Echo program, now lets play a game.
+I will ask you for an input of a specific type
+and echo your answer if it is correct.
+Due to the type checking, if you fail I fail!!!
+
+Now, give me a boolean value, represented by a number
+input: echo 1
+
+Now, give me an integer value
+input: echo 2
+
+Now, give me a char
+input: echo 2
+
+Congrats, you won the game
+"""
+
+    tests = {"functions/readtest"             : ("2\n2\n2\n",readtestRes)
+            ,"types/testTypeInferenceRead_b1" : ("3\n97\na\ng\n4\n","n = 5\nb = 1\nc = g\nb = 1\n")
+            ,"types/testTypeInferenceRead_b0" : ("2\n92\na\n?\n0\n","n = 4\nb = 0\nc = ?\nb = 0\n")
+            }
+
+    runTests(tests,"")
 
 def procTest():
-    tests = {"procSwap" : ("1\n2\n","1 2\n2 1\n")
-            ,"proctest" : ("", "3 4")
+    tests = {"procSwap"        : ("1\n2\n","1 2\n2 1\n")
+            ,"proctest"        : ("", "3 4")
+            ,"procReturnSimul" : ("g\n","g\nh\n")
+            ,"procReturnSimul" : ("3\n","3\n4\n")
             }
 
     runTests(tests,"functions/")
@@ -34,9 +61,52 @@ def arrayTest():
 
     runTests(tests,"arrays/")
 
+def ifTest():
+    ifRes = """test 1 expects result -1: ~1
+test 2 expects result 0: 0
+test 3 expects result 1: 1
+
+test 4 expects result 6: 6
+test 5 expects result -40: ~40
+test 6 expects result 15: 15
+"""
+    tests = {"dangling"   : ("","passed.\n")
+            ,"iftheltest" : ("",ifRes)
+            }
+
+    runTests(tests,"ifthenelse/")
+
+def opTest():
+    mulDivRes = """Test 1 expects 12 and was: 12
+Test 2 expects 720 and was: 720
+Test 3 expects 60 and was: 60
+"""
+
+    assocRes = """passed.
+passed.
+passed.
+passed.
+passed.
+passed.
+passed.
+passed.
+passed.
+"""
+
+    tests = {"operators/logical"    : ("","Test result should be 1. Actual result: 1")
+            ,"operators/orTest"     : ("","1")
+            ,"operators/testMulDiv" : ("",mulDivRes)
+            ,"misc/precAssoc"       : ("",assocRes)
+            }
+
+    runTests(tests,"")
+
 def main():
     tests = {"arrays"     : arrayTest
             ,"procedures" : procTest
+            ,"read"       : readTest
+            ,"if"         : ifTest
+            ,"operators"  : opTest
             }
 
     def allTests():
